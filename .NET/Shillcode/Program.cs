4d6d9de3cf9374b74dbdc955c093bf3901194248
backdoorskid <m9dwi8d8og2o@proton.me>
2026-03-08 22:31:45 +0000

    Add utility for .NET (Shillcode)

---
+++ .NET/Shillcode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+namespace Shillcode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShillcodeContext dynAsm = new ShillcodeContext("DynamicAssembly");
+            ModuleContext    dynMod = dynAsm.StartModuleDefinition("MainModule");
+
+            // 17 | ldc.i4.1
+            // 2A | ret
+            dynMod.AddMethodToModule(
+                "RetTrueStatic",
+                new byte[] { 0x17, 0x2A },
+                MethodAttributes.Public | MethodAttributes.Static,
+                typeof(bool),
+                Type.EmptyTypes
+            );
+            dynMod.AddMethodToModule(
+                "RetTrue",
+                new byte[] { 0x17, 0x2A },
+                MethodAttributes.Public,
+                typeof(bool),
+                Type.EmptyTypes
+            );
+            dynMod.EndModuleDefinition();
+
+            MethodInfo RetTrueStatic = dynMod.GetMethod("RetTrueStatic");
+            MethodInfo RetTrue       = dynMod.GetMethod("RetTrue");
+
+            Console.WriteLine(
+                "Result (RetTrueStatic): {0}",
+                RetTrueStatic.Invoke(null, null)
+            );
+            Console.WriteLine(
+                "Result (RetTrue):       {0}",
+                RetTrue.Invoke(Activator.CreateInstance(dynMod.dynamicType), null)
+            );
+        }
+    }
+}
---
+++ .NET/Shillcode/ShillcodeContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+
+namespace Shillcode
+{
+    public class ShillcodeContext
+    {
+        public AssemblyBuilder assemblyBuilder;
+
+        public ShillcodeContext(string assemblyName)
+        {
+            assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(assemblyName), AssemblyBuilderAccess.Run);
+        }
+
+        public ModuleContext StartModuleDefinition(string moduleName)
+        {
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule(moduleName);
+            var typeBuilder   = moduleBuilder.DefineType("DynamicType", TypeAttributes.Public);
+
+            return new ModuleContext
+            {
+                moduleBuilder = moduleBuilder,
+                typeBuilder   = typeBuilder,
+                methods       = new List<MethodBuilder>()
+            };
+        }
+    }
+
+    public class ModuleContext
+    {
+        public ModuleBuilder moduleBuilder;
+        public TypeBuilder typeBuilder;
+        public List<MethodBuilder> methods;
+        public Type dynamicType;
+
+        public void EndModuleDefinition()
+        {
+            dynamicType = typeBuilder.CreateType();
+        }
+
+        public void AddMethodToModule(string name, byte[] ilCode, MethodAttributes attributes, Type returnType, Type[] parameterTypes)
+        {
+            MethodBuilder methodBuilder = typeBuilder.DefineMethod(name, attributes, returnType, parameterTypes);
+            ILGenerator ilGenerator = methodBuilder.GetILGenerator();
+
+            FieldInfo ilStream = typeof(ILGenerator).GetField("m_ILStream", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo ilLength = typeof(ILGenerator).GetField("m_length", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo maxStackSize = typeof(ILGenerator).GetField("m_maxStackSize", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            ilStream.SetValue(ilGenerator, ilCode);
+            ilLength.SetValue(ilGenerator, ilCode.Length);
+            maxStackSize.SetValue(ilGenerator, int.MaxValue);
+
+            methods.Add(methodBuilder);
+        }
+
+        public MethodInfo GetMethod(string name)
+        {
+            return dynamicType.GetMethod(name);
+        }
+    }
+}
